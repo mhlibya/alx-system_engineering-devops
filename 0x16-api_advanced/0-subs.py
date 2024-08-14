9c993d2b83d5fa13d266e13cc76eb9612ffad86a
mhlibya <mhlibya728@gmail.com>
2024-08-14 02:06:02 +0200

    getting started

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers to the subreddit, or 0 if the subreddit is invalid.
     """
     headers = {'User-Agent': 'xica369'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
